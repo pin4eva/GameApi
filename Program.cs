0ea514d49f847e6b548fd6882252a3343f4f435c
Peter Akaliro <38269224+pin4eva@users.noreply.github.com>
2023-08-25 06:51:43 +0100

    first commit

---
+++ Program.cs
@@ -0,0 +1,66 @@
+using GameApi.Entities;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+List<Game> games = new()
+{
+new Game(){Id=1, Name="Street Fighter II", Genre="Fighting",Price=19.99M,ReleaseDate= new DateTime(2022, 1, 1),ImageUrl="https://placeholder.co/100"},
+new Game(){Id=2, Name="Final Fantacy XIV", Genre="RolePlaying",Price=69.99M,ReleaseDate= new DateTime(2010, 9, 3),ImageUrl="https://placeholder.co/100"},
+new Game(){Id=3, Name="FIFA 23", Genre="Fighting",Price=19.99M,ReleaseDate= new DateTime(2022, 9, 27),ImageUrl="https://placeholder.co/100"},
+};
+
+// Configu XIVre the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.MapGet("/", () => "Hello Word 1");
+app.MapGet("/games", () => games);
+app.MapGet("/games/{id}", (int id) => games.Find(game => game.Id == id));
+app.MapPost("/games", (Game game) =>
+{
+    game.Id = games.Max(game => game.Id) + 1;
+
+    games.Add(game);
+    return games;
+});
+
+app.MapPut("/games/{id}", (int id, Game game) =>
+{
+    Game? existingGame = games.Find(game => game.Id == id);
+    if (existingGame is null) return Results.NotFound("Game not found");
+
+    existingGame.Name = game.Name;
+    existingGame.Price = game.Price;
+    existingGame.ImageUrl = game.ImageUrl;
+    existingGame.ReleaseDate = game.ReleaseDate;
+
+    return Results.Ok(game);
+});
+
+app.MapDelete("/games/{id}", (int id) =>
+{
+    Game? game = games.Find(game => game.Id == id);
+    if (game is null) return Results.NotFound("game not found");
+    games.Remove(game);
+    return Results.Ok(game);
+});
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
