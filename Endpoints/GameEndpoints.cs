99787ecbfe0ba46bde50c93bf18b5bb52e29ca2a
Peter Akaliro <38269224+pin4eva@users.noreply.github.com>
2023-08-29 14:59:57 +0100

    fix: fixed update game repository

--- Endpoints/GameEndpoints.cs
+++ Endpoints/GameEndpoints.cs
@@ -46,6 +46,7 @@ public static class GameEndpoints
             {
                 Game game = new()
                 {
+                    Id = existingGame.Id,
                     Name = updatedGameDTO.Name,
                     ImageUrl = updatedGameDTO.ImageUrl,
                     Price = updatedGameDTO.Price,
